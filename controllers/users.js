55e5758e76dd5ad2629a290ae33edf1133f98850
Connor Reese <connor.reese13@gmail.com>
2020-02-27 14:55:58 +0800

    encrypted passwords

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,21 @@
 // Require
 const Users = require("../models/users");
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
 // Routes
 router.post("/signup", (req, res) => {
-  Users.find({ email: req.body.email }).then(response => {
+  Users.findOne({ email: req.body.email }).then(response => {
     if (response == null) {
+      let encrypted = bcrypt.hashSync(req.body.password, 10);
+      req.body.password = encrypted;
       Users.create(req.body).then(response => {
         res.send(response);
       });
     } else {
       res.send("Email already exists");
     }
+    //console.log(req.body);
   });
 });
 
