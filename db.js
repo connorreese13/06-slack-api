bd24964923d440ce5de736951652f232bd6c006d
Connor Reese <connor.reese13@gmail.com>
2020-02-27 13:04:35 +0800

    created channels colllection in DB

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
 // Require
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // Connection
 mongoose.connect(
-	process.env.MONGODB_URL,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	},
-	err => {
-		err
-			? () => {
-					throw err
-			  }
-			: console.log('Connected to MongoDB')
-	}
-)
+  process.env.MONGODB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  err => {
+    err
+      ? () => {
+          throw err;
+        }
+      : console.log("Connected to MongoDB");
+  }
+);
 
 // Export
-module.exports = mongoose
+module.exports = mongoose;
