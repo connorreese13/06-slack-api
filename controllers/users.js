8be1acb16e09d602538f0203077547a2d9a84d0d
Connor Reese <connor.reese13@gmail.com>
2020-02-27 14:17:29 +0800

    modified users route to check that email already exists

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,21 @@
 // Require
-const Users = require('../models/users')
-const router = require('express').Router()
+const Users = require("../models/users");
+const router = require("express").Router();
 
 // Routes
-router.post('/signup', (req, res) => {})
-router.post('/login', (req, res) => {})
+router.post("/signup", (req, res) => {
+  Users.find({ email: req.body.email }).then(response => {
+    if (response == null) {
+      Users.create(req.body).then(response => {
+        res.send(response);
+      });
+    } else {
+      res.send("Email already exists");
+    }
+  });
+});
+
+router.post("/login", (req, res) => {});
 
 // Export
-module.exports = router
+module.exports = router;
