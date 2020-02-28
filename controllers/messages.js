2a051f0a36f360059f64faad242dfb2572f19bd2
Connor Reese <connor.reese13@gmail.com>
2020-02-28 15:33:23 +0800

    added bearer token verify to message post request

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,14 +1,24 @@
 // Require
 const Messages = require("../models/messages");
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 
 // Routes
+//Modify the /messages POST request; instead of using the incoming user _id for the user field, extract the user _id from the incoming "Bearer" token and use that
 router.post("/", (req, res) => {
-  Messages.create(req.body)
-    .then(message => {
-      res.send(message);
-    })
-    .catch(err => res.send(err));
+  let token = req.headers.authorization.split(" ")[1];
+  let verify = jwt.verify(token, process.env.SECRET);
+  if (verify) {
+    req.body.user = verify._id;
+    console.log("req.body: ", req.body);
+    Messages.create(req.body)
+      .then(message => {
+        res.send(message);
+      })
+      .catch(err => res.send(err));
+  } else {
+    res.send("Not authorized");
+  }
 });
 router.get("/", (req, res) => {
   Messages.find(req.query)
--- models/users.js
+++ models/users.js
@@ -1,18 +1,19 @@
 // Require
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // Model
-module.exports = mongoose.model('users', {
-	name: {
-		type: String,
-		required: true
-	},
-	email: {
-		type: String,
-		required: true
-	},
-	password: {
-		type: String,
-		required: true
-	}
-})
+module.exports = mongoose.model("users", {
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  }
+});
