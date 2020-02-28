d63107ff44dd782eb931dc05192dd4cb5b4c6683
Connor Reese <connor.reese13@gmail.com>
2020-02-28 15:57:55 +0800

    added all router bearer token verifications

--- controllers/channels.js
+++ controllers/channels.js
@@ -1,22 +1,38 @@
 // Require
-const Channels = require('../models/channels')
-const router = require('express').Router()
+const Channels = require("../models/channels");
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 
 // Routes
-router.post('/', (req, res) => {
-	Channels.create(req.body)
-		.then(channel => {
-			res.send(channel)
-		})
-		.catch(err => res.send(err))
-})
-router.get('/', (req, res) => {
-	Channels.find(req.query)
-		.then(channels => {
-			res.send(channels)
-		})
-		.catch(err => res.send(err))
-})
+router.post("/", (req, res) => {
+  let token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.SECRET, (error, verify) => {
+    if (verify) {
+      Channels.create(req.body)
+        .then(channel => {
+          res.send(channel);
+        })
+        .catch(err => res.send(err));
+    } else {
+      res.send({ message: "Not authorized" });
+    }
+  });
+});
+
+router.get("/", (req, res) => {
+  let token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, process.env.SECRET, (error, verify) => {
+    if (verify) {
+      Channels.find(req.query)
+        .then(channels => {
+          res.send(channels);
+        })
+        .catch(err => res.send(err));
+    } else {
+      res.send({ message: "Not authorized" });
+    }
+  });
+});
 
 // Export
-module.exports = router
+module.exports = router;
